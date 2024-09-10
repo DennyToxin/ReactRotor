4dbbea3864c613667072921a5972696a7aa25635
DennyToxin <den-d.den2015@yandex.ru>
2024-09-10 12:18:50 +0300

    change 404-page add logo

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -2,10 +2,9 @@ import { Link } from "react-router-dom";
 
 export const NotFound = () => {
   return (
-    <div className="page-view flex flex-col justify-center items-center bg-bgPrimaryLt">
-      <p>Ошибка 404</p>
-      <p>Страница не найдена</p>
-      <Link to={"/"}>На главную</Link>
+    <div className="page-view flex flex-col justify-center items-center bg-bgPrimaryLt gap-4">
+      <h2 className="font-montserrat font-semibold">Страница не найдена!</h2>
+      <Link className="button" to={"/"}>На главную</Link>
     </div>
   );
 };
